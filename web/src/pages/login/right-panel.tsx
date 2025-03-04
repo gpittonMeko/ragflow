5773ed147475567b01c191282b1fef0b34813be8
Giovanni Pitton <giovanni.pitton@mekosrl.it>
2025-03-04 17:09:16 +0100

    modifica right panel

--- web/src/pages/login/right-panel.tsx
+++ web/src/pages/login/right-panel.tsx
@@ -1,6 +1,4 @@
-import { ReactComponent as Avatars } from '@/assets/svg/login-avatars.svg';
-import SvgIcon from '@/components/svg-icon';
-import { Flex, Rate, Space, Typography } from 'antd';
+import { Flex, Typography } from 'antd';
 import classNames from 'classnames';
 
 import { useTranslate } from '@/hooks/common-hooks';
@@ -12,33 +10,13 @@ const LoginRightPanel = () => {
   const { t } = useTranslate('login');
   return (
     <section className={styles.rightPanel}>
-      <SvgIcon name="login-star" width={80}></SvgIcon>
       <Flex vertical gap={40}>
-        <Title
-          level={1}
-          className={classNames(styles.white, styles.loginTitle)}
-        >
-          {t('title')}
+        <Title level={1} className={classNames(styles.white, styles.loginTitle)}>
+          Benvenuto nel portale support di SGAI
         </Title>
         <Text className={classNames(styles.pink, styles.loginDescription)}>
           {t('description')}
         </Text>
-        <Flex align="center" gap={16}>
-          <Avatars></Avatars>
-          <Flex vertical>
-            <Space>
-              <Rate disabled defaultValue={5} />
-              <span
-                className={classNames(styles.white, styles.loginRateNumber)}
-              >
-                5.0
-              </span>
-            </Space>
-            <span className={classNames(styles.pink, styles.loginRateReviews)}>
-              {t('review')}
-            </span>
-          </Flex>
-        </Flex>
       </Flex>
     </section>
   );
