8082e14b77b20935b0e070ab135be3ddf8a1d58b
Giovanni Pitton <giovanni.pitton@mekosrl.it>
2025-03-05 18:24:44 +0100

    route sistemata

--- web/src/wrappers/auth.tsx
+++ web/src/wrappers/auth.tsx
@@ -1,13 +1,22 @@
 import { useAuth } from '@/hooks/auth-hooks';
 import { redirectToLogin } from '@/utils/authorization-util';
-import { Outlet } from 'umi';
+import { Outlet, useLocation } from 'umi';
 
 export default () => {
   const { isLogin } = useAuth();
+  const location = useLocation();
+  const publicPaths = ['/login', '/login-page'];
+
   if (isLogin === true) {
     return <Outlet />;
   } else if (isLogin === false) {
-    redirectToLogin();
+    if (!publicPaths.includes(location.pathname)) {
+      redirectToLogin();
+      return <></>;
+    } else {
+      // Se siamo su una pagina pubblica, non forziamo il redirect
+      return <Outlet />;
+    }
   }
 
   return <></>;
