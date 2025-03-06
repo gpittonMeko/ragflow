87c2f38574ff8361aea98aeca4b5bfd0b036d602
Giovanni Pitton <giovanni.pitton@mekosrl.it>
2025-03-06 11:42:54 +0100

    modifica per l'auth

--- web/src/wrappers/auth.tsx
+++ web/src/wrappers/auth.tsx
@@ -1,23 +1,26 @@
-import { useAuth } from '@/hooks/auth-hooks';
+>> import { useAuth } from '@/hooks/auth-hooks';
 import { redirectToLogin } from '@/utils/authorization-util';
 import { Outlet, useLocation } from 'umi';
 
 export default () => {
-  const { isLogin } = useAuth();
-  const location = useLocation();
-  const publicPaths = ['/login', '/login-page'];
+  const { isLogin } = useAuth();
+  const location = useLocation();
+  const publicPaths = ['/login', '/login-page'];
 
-  if (isLogin === true) {
-    return <Outlet />;
-  } else if (isLogin === false) {
-    if (!publicPaths.includes(location.pathname)) {
-      redirectToLogin();
-      return <></>;
-    } else {
-      // Se siamo su una pagina pubblica, non forziamo il redirect
-      return <Outlet />;
-    }
-  }
+  if (isLogin === true) {
+    return <Outlet />;
+  } else if (isLogin === false) {
+    if (location.pathname === '/login-page') { // ADD this condition
+      return <Outlet />; // Do NOT redirect if it's login-page
+    } else if (!publicPaths.includes(location.pathname)) {
+      redirectToLogin();
+      return <></>;
+    } else {
+      // Se siamo su una pagina pubblica, non forziamo il redirect
+      return <Outlet />;
+    }
+  }
 
-  return <></>;
+  return <></>;
 };
+>>
