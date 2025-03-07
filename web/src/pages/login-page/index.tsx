f8a88bd24db2ebbf26b496ac29a69c9ed6b8d5aa
Giovanni Pitton <giovanni.pitton@mekosrl.it>
2025-03-07 16:20:50 +0100

    container? boh chat container richiamto

--- web/src/pages/login-page/index.tsx
+++ web/src/pages/login-page/index.tsx
@@ -1,4 +1,6 @@
 import React, { memo } from 'react';
+import MemoizedChatContainer from '../../chat-container';  // <----- AGGIUNGI QUESTA RIGA!
+
 import { Flex, Spin } from 'antd';
 
 // Componenti e costanti
