6934ccde811258a095264c59d2b106bfb2ba45d8
Giovanni Pitton <giovanni.pitton@mekosrl.it>
2025-10-17 19:19:41 +0200

    fix: use nginx proxy /oauth instead of direct port 8000 in login page

--- web/src/pages/login-page/index.tsx
+++ web/src/pages/login-page/index.tsx
@@ -36,7 +36,8 @@ const getBaseURL = () => {
     url.protocol = window.location.protocol;
     return url.origin + url.pathname.replace(/\/$/, '');
   }
-  return `${window.location.protocol}//${window.location.hostname}:8000`;
+  // Use nginx proxy path instead of direct port access
+  return `${window.location.protocol}//${window.location.hostname}/oauth`;
 };
 const baseURL = getBaseURL();
 
